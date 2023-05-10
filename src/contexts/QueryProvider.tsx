61b4dc301837f5dc95a6af4de879923998f3b1f2
Gustavo Gava <gustavogava123@hotmail.com>
2023-05-10 14:47:31 -0300

    chore: configured react-query lib

---
+++ src/contexts/QueryProvider.tsx
@@ -0,0 +1,8 @@
+import { QueryClientProvider, QueryClient } from "@tanstack/react-query";
+import { PropsWithChildren } from "react";
+
+const queryClient = new QueryClient();
+
+export const QueryProvider = ({ children }: PropsWithChildren) => {
+	return <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>;
+};
