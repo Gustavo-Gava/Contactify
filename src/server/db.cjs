bf1299eee2dcd9193e40bb7e0a86f0416efed961
Gustavo Gava <gustavogava123@hotmail.com>
2023-05-11 08:58:10 -0300

    chore: configured faker to generate random data

---
+++ src/server/db.cjs
@@ -0,0 +1,36 @@
+const { faker } = require("@faker-js/faker");
+
+module.exports = function () {
+	const data = { contacts: [] };
+
+	// Create 1000 users
+	for (let i = 0; i < 100; i++) {
+		const contact = {
+			id: i + 1,
+			name: faker.name.firstName(),
+			lastname: faker.name.lastName(),
+			category: faker.helpers.arrayElement(["Family", "Friends", "Work"]),
+
+			phones: [
+				{
+					id: 1,
+					number: faker.phone.number("+## (##) #####-####"),
+				},
+			],
+
+			addresses: [
+				{
+					id: 1,
+					street: faker.address.streetAddress(),
+					city: faker.address.city(),
+					state: faker.address.state(),
+					zip: faker.address.zipCode(),
+				},
+			],
+		};
+
+		data.contacts.push(contact);
+	}
+
+	return data;
+};
